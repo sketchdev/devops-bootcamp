f50400c986659884a0bd739b17340aa4dfd29140
Ryan Jensen <rjensen@sketchdev.io>
2019-05-24 15:52:54 -0500

    - allow running port to be passed in as an argument for deployment purposes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || process.argv[2] || 3000;
 const Die = require('./models/die');
 let die = new Die(9);
 
