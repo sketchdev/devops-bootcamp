175913a5c2651506b986cc14e5340244ea94f6cf
Ryan Jensen <rjensen@sketchdev.io>
2019-06-28 14:56:36 -0500

    - environmentalize flyway's db connection info

--- conf/flyway.js
+++ conf/flyway.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
   flywayArgs: {
-    url: `jdbc:postgresql://localhost/bootcamp`,
-    schemas: 'bootcampuser',
+    url: `jdbc:postgresql://${process.env.PGHOST}/${process.env.PGDATABASE}`,
+    schemas: process.env.PGUSER,
     locations: 'filesystem:sql',
-    user: 'bootcampuser',
-    password: 'abc123def456',
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
     baselineOnMigrate: true
     // sqlMigrationSuffix: '.sql',  // deprecated with the newer beta version
   },
-  version: '6.0.0-beta2'
+  version: '6.0.0-beta2' // need to pull beta for postgres 11 support; omitting this option normally pulls latest stable version
 };
