206829edca89f5406138a9b591da401dc2004802
Ryan Jensen <rjensen@sketchdev.io>
2019-06-30 17:28:16 -0500

    - implement model builder in test framework  - implement tests for data management  - add global data management helper functions to setup_test.js

--- test/controllers/roll_controller_test.js
+++ test/controllers/roll_controller_test.js
@@ -1,6 +1,9 @@
 require('../setup_test.js');
 
 describe('roll', async () => {
+  beforeEach(async () => {
+    await deleteData();
+  });
 
   // Test 1:
   //   Validate that it rolls a default number of sides and replies correctly
@@ -34,4 +37,38 @@ describe('roll', async () => {
         });
   });
 
+});
+
+describe('test data management', async () => {
+  beforeEach(async () => {
+    await deleteData();
+    company = await seedCompany();
+  });
+
+  // Test 3b:
+  //   Validate that test data management is working properly
+  it('should ensure the start of each test has a clean database', async () => {
+    let rollHistories = await db.any('SELECT * FROM roll_history;');
+    // console.log(rollHistories);
+    rollHistories.length.should.equal(0);
+  });
+
+  context('with seeded data', async() => {
+    let rollHistory;
+
+    beforeEach(async () => {
+      rollHistory = await seedRollHistory();
+    });
+
+    // Test 3c:
+    //   Validate that seed data is populated into the database before each test
+    it('should have some seeded data in the roll_history table', async () => {
+      let rollHistories = await db.any('SELECT * FROM roll_history;');
+      // console.log(rollHistories);
+      rollHistories.length.should.equal(1);
+      rollHistories[0].die_sides.should.equal(rollHistory.die_sides);
+      rollHistories[0].result.should.equal(rollHistory.result);
+      rollHistories[0].roll_time.should.equal(rollHistory.roll_time);
+    })
+  });
 });
