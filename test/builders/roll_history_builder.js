10e45065c0c2a5b856874102c6af275cd5ebc6d4
Ryan Jensen <rjensen@sketchdev.io>
2019-06-30 17:28:16 -0500

    - implement model builder in test framework  - implement tests for data management  - add global data management helper functions to setup_test.js

---
+++ test/builders/roll_history_builder.js
@@ -0,0 +1,22 @@
+class RollHistoryBuilder {
+
+  constructor(data = {}) {
+    this.die_sides = data.die_sides || 6;
+    this.result = data.result || 2;
+    this.roll_time = Date.now();
+  };
+
+  withSides(value) { this.die_sides = value; return this; }
+  withResult(value) { this.result = value; return this; }
+  withRollTime(value) { this.roll_time = value; return this; }
+
+  build() {
+    let rollHistory = {};
+    Object.keys(this).forEach((i) => {
+      rollHistory[i] = this[i];
+    });
+    return rollHistory;
+  }
+}
+
+module.exports = RollHistoryBuilder;
--- test/setup_test.js
+++ test/setup_test.js
@@ -6,3 +6,33 @@ global.app = require('../app.js').app;
 global.should = chai.should();
 global.expect = chai.expect;
 global.request = require('supertest');
+
+global.RollHistoryBuilder = require('./builders/roll_history_builder');
+
+global.deleteData = async () => {
+  let tableNames = ['roll_history'];
+  for (let tableName of tableNames) {
+    let sql = 'DELETE FROM ' + tableName;
+    await db.none(sql);
+  }
+};
+
+global.seedRollHistory = async () => {
+  return await insert('roll_history', new RollHistoryBuilder().build());
+};
+
+/***
+ *
+ * @param table - name of the table to be inserted into
+ * @param values - is the key value pair of what will be inserted into the table, keys = column name, values = data to be inserted, the column values and data values are the same
+ * ex: {name: 'some widget', description: req.body.description, num_items: req.body.count}
+ * @returns {Promise<any>}
+ */
+global.insert = async (table, values) => {
+  const columns = Object.keys(values).join(', ');
+  const data = Object.keys(values).map((key) => '${' + key + '}');
+  const bindings = Object.assign({}, values);
+  const sql = `insert into ${table}(${columns}) values(${data})`;
+
+  return await db.one(sql + 'RETURNING *', bindings);
+};
