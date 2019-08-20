79302b2adddcc1618036f2b56575df93b1acee27
Ryan Jensen <rjensen@raschnet.com>
2019-08-20 17:37:39 -0500

    - don't have the database test included in the suite from the get-go (but don't comment it out, either)  - fix deployment script

--- deploy_simple.sh
+++ deploy_simple.sh
@@ -5,22 +5,24 @@
 BOOTCAMP_USER_ID=workshopuser#
 
 
-$DEPLOYMENT_DIR="/c/mnt/deploy/$BOOTCAMP_USER_ID/devops-bootcamp/"
+DEPLOYMENT_MOUNT="../deploy"
+DEPLOYMENT_DIR="$DEPLOYMENT_MOUNT/$BOOTCAMP_USER_ID/devops-bootcamp/"
 
 # Figure out if linux/mac or windows
 case `uname -s` in
     Linux*)     type=mount;;
     Darwin*)    type=mount;;
+    CYGWIN_NT*) type=mount;;
     *)          type=manual
 esac
 
 # Ensure deployment folder is ready
-if [ ! -d "/path/to/dir" ]; then
+if [ ! -d $DEPLOYMENT_DIR ]; then
   if [ "$type" == "mount" ]; then
-    mkdir -p /c/mnt/deploy
+    mkdir -p $DEPLOYMENT_MOUNT
     apt-get update
     apt-get install -y cifs-utils
-    mount -t cifs //webserver.local/deploy /c/mnt/deploy/ -o rw,user=devopssamba,pass=devopsbootcampshare
+    mount -t cifs //webserver.local/deploy $DEPLOYMENT_MOUNT -o rw,user=devopssamba,pass=devopsbootcampshare
   else
     echo ""
     echo "If this is a Windows-based machine, please ensure that the S:\ drive is mapped and loaded to the deployment server and try again."
