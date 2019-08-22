04a7dbefbf627f057495003e6a9ffac299730a16
Ryan Jensen <rjensen@raschnet.com>
2019-08-21 23:22:44 -0500

    - add option to flyway config so migrations may be run slightly out of order  - fix undo sql so it actually drops table instead of creating one

--- conf/flyway.js
+++ conf/flyway.js
@@ -7,7 +7,8 @@ module.exports = {
     locations: 'filesystem:sql',
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
-    baselineOnMigrate: true
+    baselineOnMigrate: true,  // helps with multiple users working on the same project and committing at different times
+    outOfOrder: true
     // sqlMigrationSuffix: '.sql',  // deprecated with the newer beta version
   },
   version: '6.0.0-beta2' // need to pull beta for postgres 11 support; omitting this option normally pulls latest stable version
