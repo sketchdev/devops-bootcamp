cf456574ef5906b77e6705448c8d9c1059857bed
Ryan Jensen <rjensen@raschnet.com>
2019-08-23 11:34:15 -0500

    - adding feature toggle implementation  - roll 2 dice when toggle is on  - add unleash-client package  - add environment vars to .env needed for Unleash connectivity

--- controllers/roll_controller.js
+++ controllers/roll_controller.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Die = require('../models/die');
 const { db } = require('../lib/db');
+const { isEnabled } = require('unleash-client');
 
 router.get('/', async (req, res) => {
   let die = new Die(req.query.sides);
   let roll = die.roll();
+  let message = `A die with ${die.sides} sides just rolled a ${roll}.`;
   await saveRoll(die.sides, roll);
-  res.send(`A die with ${die.sides} sides just rolled a ${roll}.`)
+
+  if (isEnabled('rollTwo_wsu0000')) {
+    let die2 = new Die(req.query.sides);
+    let roll2 = die2.roll();
+    message = `Two dice each with ${die.sides} sides just rolled a ${roll} and ${roll2}.`;
+    await saveRoll(die2.sides, roll2);
+  }
+
+  res.send(message)
 });
 
 
