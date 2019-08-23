6c9a5f482a12a13cfd711c6723885b00d121f906
Ryan Jensen <rjensen@raschnet.com>
2019-08-23 11:34:15 -0500

    - adding feature toggle implementation  - roll 2 dice when toggle is on  - add unleash-client package  - add environment vars to .env needed for Unleash connectivity

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const { initialize } = require('unleash-client');
 require('dotenv').config();
 express = require('express');
 app = express();
 port = process.env.PORT || process.argv[2] || 3000;
+
+// initialize unleash connectivity
+initialize({
+  url: process.env.UNLEASH_URL,
+  appName: process.env.UNLEASH_APP
+});
 
 app.get('/', (req, res) => res.send('Hello DevOps Bootcamp!'));
 app.use('/roll', require('./controllers/roll_controller'));
