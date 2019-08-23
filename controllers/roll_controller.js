01e6d2eb2b94bdf108c0b6ba0c399a564c726aed
Ryan Jensen <rjensen@raschnet.com>
2019-08-23 09:57:46 -0500

    - Single-Responsibility Principle....plus, having this as a function will help keep the upcoming feature toggle exercise cleaner

--- controllers/roll_controller.js
+++ controllers/roll_controller.js
@@ -6,14 +6,18 @@ const { db } = require('../lib/db');
 router.get('/', async (req, res) => {
   let die = new Die(req.query.sides);
   let roll = die.roll();
+  await saveRoll(die.sides, roll);
+  res.send(`A die with ${die.sides} sides just rolled a ${roll}.`)
+});
+
+
+let saveRoll = async function (sides, rollResult) {
   try {
-    await db.none(`INSERT INTO roll_history(die_sides, result) VALUES (${die.sides}, ${roll});`);
+    await db.none(`INSERT INTO roll_history(die_sides, result) VALUES (${sides}, ${rollResult});`);
   } catch (e) {
     console.log('Failed to log roll history.');
     console.log(e);
   }
-  res.send(`A die with ${die.sides} sides just rolled a ${roll}.`)
-});
-
+};
 
 module.exports = router;
