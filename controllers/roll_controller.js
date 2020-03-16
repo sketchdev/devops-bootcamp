9a0228cd923d6b57f64678b3aa93b80ab863ac6f
Ryan Jensen <rjensen@raschnet.com>
2020-03-15 22:44:42 -0500

    - add winston logging package  - create centralized logger (shared formatting options)  - update .env for environment-specific logging level  - add log messages to roll_controller

--- controllers/roll_controller.js
+++ controllers/roll_controller.js
@@ -3,20 +3,25 @@ const router = express.Router();
 const Die = require('../models/die');
 const { db } = require('../lib/db');
 const { isEnabled } = require('unleash-client');
+const logger = require('../lib/logger');
 
 router.get('/', async (req, res) => {
+  const profiler = logger.startTimer();
   let die = new Die(req.query.sides);
   let roll = die.roll();
+  logger.info('Roll value obtained', { numSides: die.sides, rolled: roll });
   let message = `A die with ${die.sides} sides just rolled a ${roll}.`;
   await saveRoll(die.sides, roll);
 
   if (isEnabled('rollTwo_wsu0000')) {
     let die2 = new Die(req.query.sides);
     let roll2 = die2.roll();
+    logger.info('Roll value obtained', { numSides: die2.sides, rolled: roll2 });
     message = `Two dice each with ${die.sides} sides just rolled a ${roll} and ${roll2}.`;
     await saveRoll(die2.sides, roll2);
   }
 
+  profiler.done({ message: 'Die roll complete!' });
   res.send(message)
 });
 
@@ -25,8 +30,8 @@ let saveRoll = async function (sides, rollResult) {
   try {
     await db.none(`INSERT INTO roll_history(die_sides, result) VALUES (${sides}, ${rollResult});`);
   } catch (e) {
-    console.log('Failed to log roll history.');
-    console.log(e);
+    logger.error('Failed to log roll history.');
+    logger.error(e);
   }
 };
 
